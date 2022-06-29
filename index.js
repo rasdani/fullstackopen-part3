fca585219206400485b02a257976e2e4a633dd41
rasdani <daniel.auras94@gmx.de>
2022-06-29 16:25:23 +0200

    exercise 3.2 done

--- index.js
+++ index.js
@@ -38,6 +38,22 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  const date = new Date()
+  response.send(`Phonebook has info for ${persons.length} people <p>${date}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
