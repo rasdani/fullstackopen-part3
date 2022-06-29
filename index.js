37daa7e12cba876261cb131ef6e0e1278d1405a8
rasdani <daniel.auras94@gmx.de>
2022-06-29 16:14:41 +0200

    exercise 3.1 done

---
+++ index.js
@@ -0,0 +1,43 @@
+//const http = require('http')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+//const app = http.createServer((request, response) => {
+  //response.writeHead(200, { 'Content-Type': 'application/json' })
+  //response.end(JSON.stringify(persons))
+//})
+
+app.get('/api/persons', (request, response) => {
+  console.log('API called')
+  //response.send('<h1>Hello world</h1>') 
+  response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
