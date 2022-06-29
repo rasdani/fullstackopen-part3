88b3a4e7558d062f436c72442fc58770c1476144
rasdani <daniel.auras94@gmx.de>
2022-06-29 17:40:17 +0200

    exercise 3.5 done

--- index.js
+++ index.js
@@ -61,6 +61,19 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(request.headers)
+  console.log(request.body)
+
+  const person = request.body
+  person.id = Math.floor(Math.random() * 1000)
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
