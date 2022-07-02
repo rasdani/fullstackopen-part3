03aee586f0fdfa78a468762c1746e17896551024
rasdani <daniel.auras94@gmx.de>
2022-07-02 19:17:13 +0200

    uibuild

--- models/person.js
+++ models/person.js
@@ -24,7 +24,7 @@ const personSchema = new mongoose.Schema({
     required: true,
     validate: {
       validator: v => {
-        return /(\d{2}|\d{3})-\d+/.test(v)
+        return /(\d{2}-|\d{3}-)\d+/.test(v)
       }
     }
   }
